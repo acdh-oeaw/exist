3b551c41550a5678938238c00d06aea7a2a60337
Omar Siam <simar@gmx.net>
2022-02-15 00:14:30 +0100

    Need to move patches before switching to release

--- build-ShenGC.sh
+++ build-ShenGC.sh
@@ -1,9 +1,11 @@
 export vers=5.3.0
 docker pull gcr.io/distroless/java:11
+mkdir patches
+cp -a $vers patches/$vers
 git reset --hard
 git checkout eXist-$vers
-for patch in `ls $vers`
-do patch -p1 < $vers/$patch
+for patch in `ls patches/$vers`
+do patch -p1 < patches/$vers/$patch
 done
 mvn -V -B -q -Pdocker -DskipTests -Ddependency-check.skip=true -Ddocker.tag=$vers-java11-ShenGC -Ddocker.org=acdhch clean package
 docker run -dit --rm -p 8080:8080 --name exist-ci --rm acdhch/existdb:$vers-java11-ShenGC
