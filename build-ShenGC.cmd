9d16beb69fe6395d4d0d8d58338a5ee7607ac44c
Omar Siam <Omar.Siam@oeaw.ac.at>
2022-02-14 20:35:32 +0100

    Patches to update dependencies and change the base configuration for certain versions.

---
+++ build-ShenGC.cmd
@@ -0,0 +1,14 @@
+:: run 'set vers=5.2.0' or similar
+docker pull gcr.io/distroless/java:11
+git reset --hard
+git checkout eXist-%vers%
+for /f %%p in ('dir /b %vers%') do "C:\Program Files\Git\usr\bin\patch.exe" -p1 < %vers%\%%p
+mvn -DskipTests -Pdocker clean package
+:: The next lines need to be executed manually
+docker tag existdb/existdb:%vers% acdhch/existdb:%vers%-java11-ShenGC
+docker rmi existdb/existdb:%vers%
+docker run --rm -it -p 8080:8080 -v %cd%\logs:/exist/logs acdhch/existdb:%vers%-java11-ShenGC
+grype\grype db update
+grype\grype docker:acdhch/existdb:%vers%-java11-ShenGC --only-fixed | grep Critical
+docker login
+docker push acdhch/existdb:%vers%-java11-ShenGC
---
+++ build-ShenGC.sh
@@ -0,0 +1,11 @@
+export vers=5.3.0
+docker pull gcr.io/distroless/java:11
+git reset --hard
+git checkout eXist-$vers
+for patch in `ls $vers`
+do patch -p1 < $vers/$patch
+done
+mvn -V -B -q -Pdocker -DskipTests -Ddependency-check.skip=true -Ddocker.tag=$vers-java11-ShenGC -Ddocker.org=acdhch clean package
+docker run -dit --rm -p 8080:8080 --name exist-ci --rm acdhch/existdb:$vers-java11-ShenGC
+docker login
+mvn -q -Ddocker.tag=$vers-java11-ShenGC -Ddocker.org=acdhch -Ddocker.username=$DOCKER_USERNAME -Ddocker.password=$DOCKER_PASSWORD docker:build docker:push
