f99989444dc5451287a7a75f0132c44e7b587368
Omar Siam <simar@gmx.net>
2022-02-15 00:48:42 +0100

    Use var defaults, make maven docker push work

--- build-ShenGC.sh
+++ build-ShenGC.sh
@@ -1,4 +1,4 @@
-export vers=5.3.0
+vers=${1:-5.3.0}
 docker pull gcr.io/distroless/java:11
 mkdir patches
 cp -a $vers patches/$vers
@@ -9,5 +9,7 @@ do patch -p1 < patches/$vers/$patch
 done
 mvn -V -B -q -Pdocker -DskipTests -Ddependency-check.skip=true -Ddocker.tag=$vers-java11-ShenGC -Ddocker.org=acdhch clean package
 docker run -dit --rm -p 8080:8080 --name exist-ci --rm acdhch/existdb:$vers-java11-ShenGC
-docker login
-mvn -q -Ddocker.tag=$vers-java11-ShenGC -Ddocker.org=acdhch -Ddocker.username=$DOCKER_USERNAME -Ddocker.password=$DOCKER_PASSWORD docker:build docker:push
+#docker login
+#docker push acdhch/existdb:$vers-java11-ShenGC
+cd exist-docker
+mvn -q -Pdocker -Ddocker.tag=$vers-java11-ShenGC -Ddocker.org=acdhch -Ddocker.username=$DOCKER_USERNAME -Ddocker.password=$DOCKER_PASSWORD docker:build docker:push
