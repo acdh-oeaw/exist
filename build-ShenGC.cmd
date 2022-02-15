4d2e3a225de5140b3e5350926b8c42cd9cc97ff6
Omar Siam <simar@gmx.net>
2022-02-15 01:12:56 +0100

    Copy patches same as bash version

--- build-ShenGC.cmd
+++ build-ShenGC.cmd
@@ -1,8 +1,10 @@
 :: run 'set vers=5.2.0' or similar
 docker pull gcr.io/distroless/java:11
+mkdir patches
+xcopy /E %vers% patches\
 git reset --hard
 git checkout eXist-%vers%
-for /f %%p in ('dir /b %vers%') do "C:\Program Files\Git\usr\bin\patch.exe" -p1 < %vers%\%%p
+for /f %%p in ('dir /b patches\%vers%') do "C:\Program Files\Git\usr\bin\patch.exe" -p1 < patches\%vers%\%%p
 mvn -DskipTests -Pdocker clean package
 :: The next lines need to be executed manually
 docker tag existdb/existdb:%vers% acdhch/existdb:%vers%-java11-ShenGC
